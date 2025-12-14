c600d72f7f9983bd0042a522c8d6027b948afedd
BL4ZE <bl4ze249@gmail.com>
2025-12-14 12:56:36 +0530

    Add minor age check in Python script

---
+++ Python.py
@@ -0,0 +1,6 @@
+print("HELLO WORLD")
+name = "WORLD"
+def greet(WORLD): return f"Hello, {WORLD}!".
+for i in range(5): print(i) iterates 0-4, while while condition: ...
+if age >= 18: print("Adult") else: print("Minor")
+(age = 25)
